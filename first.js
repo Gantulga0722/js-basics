3812172c46cfe6693d46742a9303ffa69fc08879
Gantulga0722 <mgtulga22@gmail.com>
2023-11-28 09:39:05 +0800

    Angias

---
+++ first.js
@@ -0,0 +1,74 @@
+// let x = 15;
+// let y = 20;
+// if (x > y) {
+//   console.log("x is bigger");
+// } else {
+//   console.log("y is bigger");
+// }
+// let myAge = 34;
+// if (myAge > 21) {
+//   console.log("you can enter the club");
+// } else {
+//   console.log("you can not enter the club");
+// }
+// let birthYear = 2004;
+// let age = 2023 - birthYear;
+// if (age > 21) {
+//   console.log("You can enter the club");
+// } else {
+//   console.log("You can not enter the club");
+// }
+// let driverBirthYear = 2004;
+// let driverAge = 2023 - driverBirthYear;
+// if (driverAge > 18) {
+//   console.log("You are allowed to drive", "Your age is:", driverAge);
+// } else {
+//   console.log("You are not allowed to drive", "Your age is:", driverAge);
+// }
+// let day = 9;
+// if (day == 1) {
+//   console.log("Today is Monday");
+// } else if (day == 2) {
+//   console.log("Today is Tuesday");
+// } else if (day == 3) {
+//   console.log("Today is Wednesday");
+// } else if (day == 4) {
+//   console.log("Today is Tirsday");
+// } else if (day == 5) {
+//   console.log("Today is Friday");
+// } else if (day == 6) {
+//   console.log("Today is Saturday");
+// } else if (day == 7) {
+//   console.log("Today is Sanday");
+// } else {
+//   console.log("Today is not a day of a week");
+// }
+
+let days = 45;
+if (days < 31) {
+  console.log("This month is Janurary");
+} else if (days < 59) {
+  console.log("This month is Feburary");
+} else if (days < 90) {
+  console.log("This month is March");
+} else if (days < 120) {
+  console.log("This month is April");
+} else if (days < 151) {
+  console.log("This month is May");
+} else if (days < 181) {
+  console.log("This month is June");
+} else if (days < 212) {
+  console.log("This month is July");
+} else if (days < 243) {
+  console.log("This month is August");
+} else if (days < 273) {
+  console.log("This month is September");
+} else if (days < 304) {
+  console.log("This month is October");
+} else if (days < 334) {
+  console.log("This month is Novenber");
+} else if (days < 365) {
+  console.log("This month is December");
+} else {
+  console.log("The days are more than a year");
+}
---
+++ first1.js
@@ -0,0 +1,64 @@
+let days = 45;
+if (days < 31) {
+  console.log("This month is Janurary");
+} else if (days < 59) {
+  console.log("This month is Feburary");
+} else if (days < 90) {
+  console.log("This month is March");
+} else if (days < 120) {
+  console.log("This month is April");
+} else if (days < 151) {
+  console.log("This month is May");
+} else if (days < 181) {
+  console.log("This month is June");
+} else if (days < 212) {
+  console.log("This month is July");
+} else if (days < 243) {
+  console.log("This month is August");
+} else if (days < 273) {
+  console.log("This month is September");
+} else if (days < 304) {
+  console.log("This month is October");
+} else if (days < 334) {
+  console.log("This month is Novenber");
+} else if (days < 365) {
+  console.log("This month is December");
+} else {
+  console.log("The days are more than a year");
+}
+
+let midterm1 = 85;
+let midterm2 = 85;
+let final = 95;
+let score = midterm1 * 0.25 + midterm2 * 0.25 + final * 0.5;
+if (score < 60) {
+  console.log("Your score is:", score, "F");
+} else if (score < 70) {
+  console.log("Your score is:", score, "D");
+} else if (score < 80) {
+  console.log("Your score is:", score, "C");
+} else if (score < 90) {
+  console.log("Your score is:", score, "B");
+} else if (score < 100) {
+  console.log("Your score is:", score, "A");
+} else {
+  console.log("Error: Wrong score");
+}
+
+let midterm1 = 80;
+let midterm2 = 80;
+let final = 95;
+let score = (midterm1 + midterm2 + final) / 3;
+if (score < 60) {
+  console.log("Your score is:", score, "F");
+} else if (score < 70) {
+  console.log("Your score is:", score, "D");
+} else if (score < 80) {
+  console.log("Your score is:", score, "c");
+} else if (score < 90) {
+  console.log("Your score is:", score, "B");
+} else if (score < 100) {
+  console.log("Your score is:", score, "A");
+} else {
+  console.log("Error: This is wrong score");
+}

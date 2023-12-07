379febc07d940ef15cb45a169bf3538611a006e8
Гантулга Мөнхдэлгэр <gantulga@coremind.mn>
2023-12-07 14:24:05 +0800

    Ajlaas

--- exercise/exerclass3.js
+++ exercise/exerclass3.js
@@ -1,10 +1,18 @@
 function findSome(x, y, z) {
   let some = [];
   count = -1;
-  for (let i = x; i <= y; i = i + z) {
-    count++;
-    some[count] = i;
+  if (x < y) {
+    for (let i = x; i <= y; i = i + z) {
+      count++;
+      some[count] = i;
+    }
+    console.log(some);
+  } else {
+    for (let a = y; a <= x; a = a + z) {
+      count++;
+      some[count] = a;
+    }
+    console.log(some);
   }
-  console.log(some);
 }
-findSome(10, 20, 2);
+findSome(20, 10, 2);

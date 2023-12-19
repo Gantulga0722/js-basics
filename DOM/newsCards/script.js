7fe07da3e0c5c9baa57e15008f08d26a6d7537c5
Gantulga0722 <mgtulga22@gmail.com>
2023-12-20 03:27:05 +0800

    Gerees

---
+++ DOM/newsCards/script.js
@@ -0,0 +1,214 @@
+const newsArr = [
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+  {
+    title: `Нийслэлийн шатар сонирхогчдын аваргууд шалгарчээ`,
+    image:
+      "https://mgl.gogo.mn/newsn/thumbnail/420/images/c/2023/12/313967-19122023-1702986526-2091305073-shatar_avarga.jpg",
+    description: `Монгол Улсын Үндсэн хуулийн өдрийг угтаж Нийслэлийн ИТХ-ын ивээл дор “Шатар сонирхогчдын 2023 оны Нийслэлийн аварга шалгаруулах тэмцээн”-ий хагас шигшээ шатыг Хурлын Ажлын алба Нийслэлийн Шатрын холбо`,
+    share: "1",
+    comment: "23",
+    time: ` 53 минутын өмнө`,
+  },
+];
+
+const root = document.getElementById("root");
+function newsCard(news) {
+  const newsCards = document.createElement("div");
+
+  newsCards.setAttribute("class", "news_card");
+
+  const tncImg = document.createElement("div");
+  tncImg.setAttribute("class", "tnc_img");
+  const hovDiv = document.createElement("div");
+  const hovDivP = document.createElement("p");
+  hovDivP.innerText = "Унших";
+  hovDiv.appendChild(hovDivP);
+  tncImg.appendChild(hovDiv);
+  const tncimg1 = document.createElement("img");
+  tncimg1.setAttribute("src", news.image);
+  tncImg.appendChild(tncimg1);
+  newsCards.appendChild(tncImg);
+
+  const tncText = document.createElement("div");
+  tncText.setAttribute("class", "tnc_text");
+  const tncTextH3 = document.createElement("h3");
+  tncTextH3.innerText = news.title;
+  const tncTextP = document.createElement("p");
+  tncTextP.innerText = news.description;
+  tncText.appendChild(tncTextH3);
+  tncText.appendChild(tncTextP);
+
+  const tncIcon = document.createElement("div");
+  tncIcon.setAttribute("class", "tnc_icon");
+
+  const shareIcon = document.createElement("div");
+  shareIcon.setAttribute("class", "share_icon");
+  const iconIcon1 = document.createElement("i");
+  iconIcon1.setAttribute("src", "");
+  const iconSpan1 = document.createElement("span");
+  iconSpan1.innerText = news.share;
+  shareIcon.appendChild(iconIcon1);
+  shareIcon.appendChild(iconSpan1);
+
+  const chatIcon = document.createElement("div");
+  chatIcon.setAttribute("class", "chat_icon");
+  const iconImg2 = document.createElement("img");
+  iconImg2.setAttribute(
+    "src",
+    "https://mgl.gogo.mn/newsn/v4/images/comment-light.81abc418.svg"
+  );
+  const iconSpan2 = document.createElement("span");
+  iconSpan2.innerText = news.comment;
+  chatIcon.appendChild(iconImg2);
+  chatIcon.appendChild(iconSpan2);
+
+  const timeIcon = document.createElement("div");
+  timeIcon.setAttribute("class", "time_icon");
+  const iconImg3 = document.createElement("img");
+  iconImg3.setAttribute(
+    "src",
+    "https://mgl.gogo.mn//newsn/v4/images/time-gray.9703a3cc.svg"
+  );
+  const iconSpan3 = document.createElement("span");
+  iconSpan3.innerText = news.time;
+  tncText.appendChild(tncIcon);
+  timeIcon.appendChild(iconImg3);
+  timeIcon.appendChild(iconSpan3);
+
+  tncIcon.appendChild(shareIcon);
+  tncIcon.appendChild(chatIcon);
+  tncIcon.appendChild(timeIcon);
+  newsCards.appendChild(tncImg);
+  newsCards.appendChild(tncText);
+  return newsCards;
+}
+
+newsArr.map((new1) => {
+  let toReadNews = newsCard(new1);
+  root.appendChild(toReadNews);
+});

b4541c9e24f4c95102ff22c4987bd0f6058384bd
Gantulga0722 <mgtulga22@gmail.com>
2023-12-25 23:37:41 +0800

    From Home

---
+++ DOM/todolist/script.js
@@ -0,0 +1,97 @@
+const root = document.getElementById("root");
+
+const header = document.createElement("header");
+root.appendChild(header);
+
+const headerHeading = document.createElement("h1");
+header.appendChild(headerHeading);
+headerHeading.innerText = "To-Do List";
+
+const container = document.createElement("div");
+container.setAttribute("class", "input_container");
+root.appendChild(container);
+
+const heading = document.createElement("h1");
+container.appendChild(heading);
+heading.innerText = "To-Do List";
+
+const tasks = document.createElement("div");
+tasks.setAttribute("class", "tasks");
+container.appendChild(tasks);
+
+const taskInput = document.createElement("input");
+taskInput.setAttribute("class", "task_input");
+taskInput.setAttribute("placeholder", "Enter task here...");
+tasks.appendChild(taskInput);
+
+const addTaskButton = document.createElement("button");
+addTaskButton.setAttribute("class", "addTask_btn");
+tasks.appendChild(addTaskButton);
+addTaskButton.innerText = "Add Task";
+addTaskButton.addEventListener("click", addTask);
+
+const tasksContainer = document.createElement("div");
+tasksContainer.setAttribute("class", "task_container");
+root.appendChild(tasksContainer);
+
+function addTask() {
+  localStorage.setItem("task", taskInput.value);
+  const taskDiv = document.createElement("div");
+  taskDiv.setAttribute("class", "added_task");
+  tasksContainer.appendChild(taskDiv);
+
+  const checkAndLabel = document.createElement("div");
+  checkAndLabel.setAttribute("class", "check_label");
+  taskDiv.appendChild(checkAndLabel);
+
+  const taskCheckbox = document.createElement("input");
+  taskCheckbox.select("class", "task_checkbox");
+  taskCheckbox.setAttribute("type", "checkbox");
+  taskCheckbox.setAttribute("id", "checkboxId");
+  checkAndLabel.appendChild(taskCheckbox);
+  taskCheckbox.addEventListener("click", doneTask);
+
+  const formLabel = document.createElement("form");
+  formLabel.setAttribute("action", "");
+  checkAndLabel.appendChild(formLabel);
+
+  const taskLabel = document.createElement("input");
+  taskLabel.setAttribute("class", "task_label");
+  taskLabel.setAttribute("id", "task_label");
+  taskLabel.setAttribute("type", "text");
+  taskLabel.setAttribute("disabled", "disbaled");
+  formLabel.appendChild(taskLabel);
+  taskLabel.value = localStorage.getItem("task");
+
+  const addedTaskBtns = document.createElement("div");
+  addedTaskBtns.setAttribute("class", "addedTask_btns");
+  taskDiv.appendChild(addedTaskBtns);
+
+  const editButton = document.createElement("button");
+  editButton.setAttribute("class", "task_buttons");
+  editButton.innerText = "Edit";
+  addedTaskBtns.appendChild(editButton);
+  editButton.addEventListener("click", editTask);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("class", "task_buttons");
+  deleteButton.innerText = "Delete";
+  addedTaskBtns.appendChild(deleteButton);
+  deleteButton.addEventListener("click", deleteTask);
+
+  function editTask() {
+    if (document.getElementById("task_label").disabled == true) {
+      document.getElementById("task_label").disabled = false;
+    }
+  }
+
+  function doneTask() {
+    if (document.getElementById("checkboxId").checked == true) {
+      document.getElementById("task_label").style.color = "green";
+    }
+  }
+
+  function deleteTask() {
+    taskDiv.innerHTML = "";
+  }
+}
